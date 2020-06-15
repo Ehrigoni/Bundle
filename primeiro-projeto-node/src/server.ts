4b7e8a1ca93f06452cef55be00a37eb7c2f2fbaa
Eduardo Henrique Rigoni <ehrigoni@gmail.com>
2020-06-15 12:33:30 -0300

    commit

--- primeiro-projeto-node/src/server.ts
+++ primeiro-projeto-node/src/server.ts
@@ -1,10 +1,12 @@
-import express from 'express';
+import express, { response } from 'express';
 import routes from './routes';
 
 const app = express();
 
+app.use(express.json());
+
 app.use(routes);
 
 app.listen(3333, () => {
-  console.log('Backend running on port 3333!');
+    return console.log("Hello");
 });
